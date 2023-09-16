fbec0fc1a54cad681c109f06315e399f06992a5b
Jorge Pardo <hi@jorgepardo.dev>
2023-09-16 12:09:35 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jorge Pardo on 16/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
